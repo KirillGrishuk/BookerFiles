9dd4263be2bf610d5f30fb88d0ba1d7873f30ed9
user <yarosh.catherine@gmail.com>
2018-12-04 17:41:04 +0300

    edit code

--- Code/BookerFiles/src/main/java/ch/makery/address/view/FileEditDialogController.java
+++ Code/BookerFiles/src/main/java/ch/makery/address/view/FileEditDialogController.java
@@ -16,11 +16,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-/**
- * Dialog to edit details of a fileM.
- *
- * @author Marco Jakob
- */
 public class FileEditDialogController {
     @FXML
     private TextField DateField;
